8d12494b01a47997c31b6677ce509f04c4d50034
Kamil <cieslakkam@gmail.com>
2020-06-19 21:57:27 +0200

    add "Car" Model

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    ENGINE = [
+        ('PB', 'Petrol'),
+        ('ON', 'Diesel'),
+        ('HY', 'Hybrid'),
+        ('EL', 'Electric'),
+    ]
+    WHEEL_DRIVE = [
+        ('FR', 'Front'),
+        ('RE', 'Rear'),
+        ('FO', 'Four')
+    ]
+    brand = models.CharField(max_length=55)
+    model = models.CharField(max_length=55)
+    engine = models.CharField(choices=ENGINE, max_length=2)
+    wheel_driver = models.CharField(choices=WHEEL_DRIVE, max_length=2)
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.brand + ' ' + self.model
